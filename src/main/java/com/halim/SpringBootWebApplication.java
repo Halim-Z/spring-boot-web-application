5530fb34257742d6307f4ec2732aa0c5812fce94
zaaim halim <zaaim1halim@gmail.com>
2020-05-16 13:49:37 +0100

    added comment

--- src/main/java/com/halim/SpringBootWebApplication.java
+++ src/main/java/com/halim/SpringBootWebApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+//the main class
 public class SpringBootWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
